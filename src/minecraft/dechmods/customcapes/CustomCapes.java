bba33b080be6820917466c24039730a289c4d623
Joel M <jmjmjm439@gmail.com>
2013-11-12 20:09:26 +0100

    no message

--- src/minecraft/dechmods/customcapes/CustomCapes.java
+++ src/minecraft/dechmods/customcapes/CustomCapes.java
@@ -1,5 +1,28 @@
 package dechmods.customcapes;
 
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLLoadEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.network.NetworkMod;
+
+@Mod(modid = "ccapes", name = "Custom Capes", version = "0.0.0.1a_dev")
+@NetworkMod(clientSideRequired = true, serverSideRequired = false)
 public class CustomCapes
 {
+    @EventHandler
+    public void preLoad(FMLPreInitializationEvent event)
+    {
+    }
+    
+    @EventHandler
+    public void mainLoad(FMLLoadEvent event)
+    {
+    }
+    
+    @EventHandler
+    public void postLoad(FMLPostInitializationEvent event)
+    {
+    }
 }
