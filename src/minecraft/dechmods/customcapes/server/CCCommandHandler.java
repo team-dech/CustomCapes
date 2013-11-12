6050ca4079b4b1d15d1231e556214f81f8973e41
Joel M <jmjmjm439@gmail.com>
2013-11-12 22:17:20 +0100

    basic command handling structure

--- src/minecraft/dechmods/customcapes/server/CCCommandHandler.java
+++ src/minecraft/dechmods/customcapes/server/CCCommandHandler.java
@@ -2,6 +2,8 @@ package dechmods.customcapes.server;
 
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.ICommandSender;
+import net.minecraft.util.ChatMessageComponent;
+import net.minecraft.util.EnumChatFormatting;
 
 public class CCCommandHandler extends CommandBase
 {
@@ -10,16 +12,65 @@ public class CCCommandHandler extends CommandBase
     {
         return "cc";
     }
-
+    
     @Override
     public String getCommandUsage(ICommandSender sender)
     {
-        return "/cc < clear | set <URL> [player] >";
+        return "/cc [ clear [player] | set <URL> [player] ]";
     }
-
+    
     @Override
     public void processCommand(ICommandSender sender, String[] args)
     {
-        // TODO Auto-generated method stub
-    }   
+        if (args.length == 0)
+        {
+            sendUsage(sender);
+        }
+        else
+        {
+            if (args.length == 1 && args[0].equals("clear")) // clear
+            {
+                // doClear
+            }
+            else if (args.length == 2) // clear [player] / set <URL>
+            {
+                String arg1 = args[0], arg2 = args[1];
+                
+                if (arg1.equals("clear"))
+                {
+                    // doPlayerClear 
+                }
+                else if (arg1.equals("set"))
+                {
+                    // doSetStuff
+                }
+                else
+                {
+                    sendUsage(sender);
+                }
+            }
+            else if (args.length == 3) // set <URL> [player]
+            {
+                String arg1 = args[0], arg2 = args[1], arg3 = args[2];
+                
+                if (arg1.equals("set")/* && maybe test for player / url */)
+                {
+                    
+                }
+                else
+                {
+                    sendUsage(sender);
+                }
+            }
+            else
+            {
+                sendUsage(sender);
+            }
+        }
+    }
+    
+    public void sendUsage(ICommandSender ics)
+    {
+        ics.sendChatToPlayer(ChatMessageComponent.createFromText(EnumChatFormatting.DARK_RED + "USAGE: " + getCommandUsage(ics)));
+    }
 }
